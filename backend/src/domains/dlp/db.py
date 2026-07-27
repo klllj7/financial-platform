db984962c6cb66ae161e874a4e5ae5d6406f4afa
jiyoon <ljy1785@naver.com>
2026-07-27 11:12:26 +0900

    db.py수정

--- backend/src/domains/dlp/db.py
+++ backend/src/domains/dlp/db.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "postgresql://fpuser:fppassword@localhost:5432/financial_platform"
+# DATABASE_URL = "postgresql://postgres:postgres1234@localhost:5432/financial_platform"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
